88e487a7fee72fd27f2eb504036f4d7b937cf40e
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-07-25 15:41:05 -0500

    touch up

--- ModNameGoesHere/SVGImporter.cs
+++ ModNameGoesHere/SVGImporter.cs
@@ -48,8 +48,8 @@ namespace SVGImporter
 
                 if (normalizedExtension == "svg" && BlenderInterface.IsAvailable)
                 {
-                    var blenderTarget = Path.Combine(trueCachePath, $"{modelName}_v2_{time}.glb");
-                    SVGToGLTF(model, blenderTarget);
+                    var blenderTarget = Path.Combine(trueCachePath, $"{modelName}_v2_{time}.glb").Replace("\'", "/");
+                    SVGToGLB(model, blenderTarget);
                     __result = blenderTarget;
                     return;
                 }
@@ -61,9 +61,10 @@ namespace SVGImporter
                 return input.Any(c => c > MaxAnsiCode);
             }
 
-            private static void SVGToGLTF(string input, string output)
+            private static void SVGToGLB(string input, string output)
             {
-                RunBlenderScript($"import bpy\nbpy.ops.import_curve.svg(filepath = '{input}')\nobjs = bpy.data.objects\ntry:\n  objs.remove(objs['Cube'], do_unlink = True)\nexcept:\n  pass\nfor obj in bpy.data.objects:\n   if type(obj.data) == 'Curve':\n     bpy.context.view_layer.objects.active = obj\n       bpy.ops.object.convert(target = 'MESH')\nbpy.ops.export_scene.gltf(filepath = '{output}'))");
+                // Yes, deleting the default cube is necessary for this script to function
+                RunBlenderScript($"import bpy\nbpy.ops.import_curve.svg(filepath = '{input}')\nobjs = bpy.data.objects\ntry:    objs.remove(objs['Cube'], do_unlink = True)\nexcept:    pass\nfor obj in bpy.data.objects:\n    if type(obj.data) == 'Curve':\n        bpy.context.view_layer.objects.active = obj\n        bpy.ops.object.convert(target = 'MESH')\nbpy.ops.export_scene.gltf(filepath = '{output}')");
             }
 
             private static void RunBlenderScript(string script, string arguments = "-b -P \"{0}\"")
@@ -72,13 +73,24 @@ namespace SVGImporter
                 File.WriteAllText(tempBlenderScript, script);
                 var blenderArgs = string.Format(arguments, tempBlenderScript);
                 blenderArgs = "--disable-autoexec " + blenderArgs;
-                Process.Start(new ProcessStartInfo(BlenderInterface.Executable, blenderArgs)
-                {
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    CreateNoWindow = true,
-                    UseShellExecute = false
-                }).WaitForExit();
+
+                var process = new Process();
+                process.StartInfo.FileName = BlenderInterface.Executable;
+                process.StartInfo.Arguments = blenderArgs;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.OutputDataReceived += OnOutput;
+                process.Start();
+                process.BeginOutputReadLine();
+                process.WaitForExit();
+
                 File.Delete(tempBlenderScript);
+            }
+            private static void OnOutput(object sender, DataReceivedEventArgs e)
+            {
+                Msg(e.Data);
             }
         }
     }
