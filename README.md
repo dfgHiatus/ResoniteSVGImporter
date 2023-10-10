ce604a5de4ff534d200831ec03356591d7cad19b
dfgHiatus <evansBM@protonmail.com>
2023-10-10 08:15:53 -0700

    Update for Resonite

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # SVGImporter
 
-A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that enables the import of SVG (Scalable Vector Graphics) files as 3D models.
+A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/) that enables the import of SVG (Scalable Vector Graphics) files as 3D models.
 
 ## Installation
-1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
-1. Place [SVGImporter.dll](https://github.com/dfgHiatus/NeosSVGImporter/releases/download/v1.0.0/SVGImporter.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
-1. If you do not have [Blender](https://www.blender.org/download/) installed to your C Drive (or Linux equivalent), install and extract it to your Neos tools folder. This can be found under `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\tools\Blender` for a default install.
-1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
+1. Install [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader).
+1. Place [SVGImporter.dll](https://github.com/dfgHiatus/ResoniteSVGImporter/releases/download/v1.0.0/SVGImporter.dll) into your `rml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods` for a default install. You can create it if it's missing, or if you launch the game once with ResoniteModLoader installed it will create the folder for you.
+1. If you do not have [Blender](https://www.blender.org/download/) installed to your C Drive (or Linux equivalent), install and extract it to your Resonite tools folder. This can be found under `C:\Program Files (x86)\Steam\steamapps\common\Resonite\tools\Blender` for a default install.
+1. Start the game. If you want to verify that the mod is working you can check your Resonite logs.
 
 Tested only on Windows, should work on Linux. Depending on the size of the file, it may take up to a few minutes to fully import. It's OK if the import dialogue seems to hang on "Preimporting" for a little bit - this is where the bulk of the conversion is done.
--- SVGImporter/SVGImporter.cs
+++ SVGImporter/SVGImporter.cs
@@ -4,94 +4,95 @@ using System.Linq;
 using System.Diagnostics;
 using System.Collections.Generic;
 using FrooxEngine;
-using BaseX;
-using CodeX;
+using Elements.Core;
+using Elements.Assets;
 using HarmonyLib;
-using NeosModLoader;
+using ResoniteModLoader;
 
-namespace SVGImporter
+namespace SVGImporter;
+
+public class SVGImporter : ResoniteMod
 {
-    public class SVGImporter : NeosMod
+    public override string Name => "SVGImporter";
+    public override string Author => "dfgHiatus";
+    public override string Version => "2.0.0";
+    public override string Link => "https://github.com/dfgHiatus/https://github.com/dfgHiatus/ResoniteSVGImporter/";
+    public static ModConfiguration config;
+    private const string SVG_FILE_EXTENSION = "svg";
+
+    public override void OnEngineInit()
     {
-        public override string Name => "SVGImporter";
-        public override string Author => "dfgHiatus";
-        public override string Version => "1.0.0";
-        public override string Link => "https://github.com/dfgHiatus/https://github.com/dfgHiatus/NeosSVGImporter/";
-        public static ModConfiguration config;
-        public override void OnEngineInit()
+        new Harmony("net.dfgHiatus.SVGImporter").PatchAll();
+        config = GetConfiguration();
+        Engine.Current.RunPostInit(() => AssetPatch());
+    }
+
+    public static void AssetPatch()
+    {
+        var aExt = Traverse.Create(typeof(AssetHelper)).Field<Dictionary<AssetClass, List<string>>>("associatedExtensions");
+        aExt.Value[AssetClass.Model].Add(SVG_FILE_EXTENSION);
+    }
+
+    [HarmonyPatch(typeof(ModelPreimporter), "Preimport")]
+    public class FileImporterPatch
+    {
+        public static void Postfix(ref string __result, string model, string tempPath)
         {
-            new Harmony("net.dfgHiatus.SVGImporter").PatchAll();
-            config = GetConfiguration();
-            Engine.Current.RunPostInit(() => AssetPatch());
+            var modelName = Path.GetFileNameWithoutExtension(model);
+            if (ContainsUnicodeCharacter(modelName))
+            {
+                throw new ArgumentException("Imported model cannot have unicode characters in its file name.");
+            }
+
+            var normalizedExtension = Path.GetExtension(model).Replace(".", "").ToLower();
+            var trueCachePath = Path.Combine(Engine.Current.CachePath, "Cache");
+            var time = DateTime.Now.Ticks.ToString();
+
+            if (normalizedExtension == SVG_FILE_EXTENSION && BlenderInterface.IsAvailable)
+            {
+                var blenderTarget = Path.Combine(trueCachePath, $"{modelName}_v2_{time}.glb").Replace("\'", "/");
+                SVGToGLB(model, blenderTarget);
+                __result = blenderTarget;
+                return;
+            }
         }
 
-        public static void AssetPatch()
+        private static bool ContainsUnicodeCharacter(string input)
         {
-            var aExt = Traverse.Create(typeof(AssetHelper)).Field<Dictionary<AssetClass, List<string>>>("associatedExtensions");
-            aExt.Value[AssetClass.Model].Add("svg");
+            const int MaxAnsiCode = 255;
+            return input.Any(c => c > MaxAnsiCode);
         }
 
-        [HarmonyPatch(typeof(ModelPreimporter), "Preimport")]
-        public class FileImporterPatch
+        private static void SVGToGLB(string input, string output)
         {
-            public static void Postfix(ref string __result, string model, string tempPath)
-            {
-                var modelName = Path.GetFileNameWithoutExtension(model);
-                if (ContainsUnicodeCharacter(modelName))
-                {
-                    throw new ArgumentException("Imported model cannot have unicode characters in its file name.");
-                }
+            // Yes, deleting the default cube is necessary for this script to function
+            RunBlenderScript($"import bpy\nbpy.ops.import_curve.svg(filepath = '{input}')\nobjs = bpy.data.objects\ntry:    objs.remove(objs['Cube'], do_unlink = True)\nexcept:    pass\nfor obj in bpy.data.objects:\n    if type(obj.data) == 'Curve':\n        bpy.context.view_layer.objects.active = obj\n        bpy.ops.object.convert(target = 'MESH')\nbpy.ops.export_scene.gltf(filepath = '{output}')");
+        }
 
-                var normalizedExtension = Path.GetExtension(model).Replace(".", "").ToLower();
-                var trueCachePath = Path.Combine(Engine.Current.CachePath, "Cache");
-                var time = DateTime.Now.Ticks.ToString();
+        private static void RunBlenderScript(string script, string arguments = "-b -P \"{0}\"")
+        {
+            var tempBlenderScript = Path.Combine(Path.GetTempPath(), Path.GetTempFileName() + ".py");
+            File.WriteAllText(tempBlenderScript, script);
+            var blenderArgs = string.Format(arguments, tempBlenderScript);
+            blenderArgs = "--disable-autoexec " + blenderArgs;
 
-                if (normalizedExtension == "svg" && BlenderInterface.IsAvailable)
-                {
-                    var blenderTarget = Path.Combine(trueCachePath, $"{modelName}_v2_{time}.glb").Replace("\'", "/");
-                    SVGToGLB(model, blenderTarget);
-                    __result = blenderTarget;
-                    return;
-                }
-            }
+            var process = new Process();
+            process.StartInfo.FileName = BlenderInterface.Executable;
+            process.StartInfo.Arguments = blenderArgs;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.OutputDataReceived += OnOutput;
+            process.Start();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
 
-            private static bool ContainsUnicodeCharacter(string input)
-            {
-                const int MaxAnsiCode = 255;
-                return input.Any(c => c > MaxAnsiCode);
-            }
-
-            private static void SVGToGLB(string input, string output)
-            {
-                // Yes, deleting the default cube is necessary for this script to function
-                RunBlenderScript($"import bpy\nbpy.ops.import_curve.svg(filepath = '{input}')\nobjs = bpy.data.objects\ntry:    objs.remove(objs['Cube'], do_unlink = True)\nexcept:    pass\nfor obj in bpy.data.objects:\n    if type(obj.data) == 'Curve':\n        bpy.context.view_layer.objects.active = obj\n        bpy.ops.object.convert(target = 'MESH')\nbpy.ops.export_scene.gltf(filepath = '{output}')");
-            }
-
-            private static void RunBlenderScript(string script, string arguments = "-b -P \"{0}\"")
-            {
-                var tempBlenderScript = Path.Combine(Path.GetTempPath(), Path.GetTempFileName() + ".py");
-                File.WriteAllText(tempBlenderScript, script);
-                var blenderArgs = string.Format(arguments, tempBlenderScript);
-                blenderArgs = "--disable-autoexec " + blenderArgs;
-
-                var process = new Process();
-                process.StartInfo.FileName = BlenderInterface.Executable;
-                process.StartInfo.Arguments = blenderArgs;
-                process.StartInfo.RedirectStandardOutput = true;
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.CreateNoWindow = true;
-                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                process.OutputDataReceived += OnOutput;
-                process.Start();
-                process.BeginOutputReadLine();
-                process.WaitForExit();
-
-                File.Delete(tempBlenderScript);
-            }
-            private static void OnOutput(object sender, DataReceivedEventArgs e)
-            {
-                Msg(e.Data);
-            }
+            File.Delete(tempBlenderScript);
+        }
+        private static void OnOutput(object sender, DataReceivedEventArgs e)
+        {
+            Msg(e.Data);
         }
     }
 }
