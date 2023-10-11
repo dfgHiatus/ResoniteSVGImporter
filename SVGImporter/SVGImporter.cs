f62d903e6da38fe2f706170ae85ac1c721dc40d9
dfgHiatus <evansBM@protonmail.com>
2023-10-11 09:42:30 -0700

    Release

--- SVGImporter/SVGImporter.cs
+++ SVGImporter/SVGImporter.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Diagnostics;
-using System.Collections.Generic;
-using FrooxEngine;
+﻿using Elements.Assets;
 using Elements.Core;
-using Elements.Assets;
+using FrooxEngine;
 using HarmonyLib;
 using ResoniteModLoader;
+using SkyFrost.Base;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SVGImporter;
 
@@ -16,45 +17,98 @@ public class SVGImporter : ResoniteMod
     public override string Name => "SVGImporter";
     public override string Author => "dfgHiatus";
     public override string Version => "2.0.0";
-    public override string Link => "https://github.com/dfgHiatus/https://github.com/dfgHiatus/ResoniteSVGImporter/";
-    public static ModConfiguration config;
+    public override string Link => "https://github.com/dfgHiatus/ResoniteSVGImporter/";
+    public static ModConfiguration Config;
+    private static readonly string TrueCachePath = Path.Combine(Engine.Current.CachePath, "Cache");
     private const string SVG_FILE_EXTENSION = "svg";
+
+    [AutoRegisterConfigKey]
+    internal static readonly ModConfigurationKey<bool> Enabled =
+        new("enabled", "Enabled", () => true);
 
     public override void OnEngineInit()
     {
         new Harmony("net.dfgHiatus.SVGImporter").PatchAll();
-        config = GetConfiguration();
-        Engine.Current.RunPostInit(() => AssetPatch());
+        Config = GetConfiguration();
+        Engine.Current.OnReady += AssetPatch;
     }
 
     public static void AssetPatch()
     {
-        var aExt = Traverse.Create(typeof(AssetHelper)).Field<Dictionary<AssetClass, List<string>>>("associatedExtensions");
-        aExt.Value[AssetClass.Model].Add(SVG_FILE_EXTENSION);
+        var aExt = Traverse.
+            Create(typeof(AssetHelper)).
+            Field<Dictionary<AssetClass, List<string>>>("associatedExtensions");
+        aExt.Value[AssetClass.Special].Add(SVG_FILE_EXTENSION);
     }
 
-    [HarmonyPatch(typeof(ModelPreimporter), "Preimport")]
-    public class FileImporterPatch
+    [HarmonyPatch(typeof(UniversalImporter), "Import", typeof(AssetClass), typeof(IEnumerable<string>),
+    typeof(World), typeof(float3), typeof(floatQ), typeof(bool))]
+    public class UniversalImporterPatch
     {
-        public static void Postfix(ref string __result, string model, string tempPath)
+        public static bool Prefix(ref IEnumerable<string> files, 
+            World world, float3 position, floatQ rotation)
         {
-            var modelName = Path.GetFileNameWithoutExtension(model);
-            if (ContainsUnicodeCharacter(modelName))
+            // If Blender is not available, we can't import SVGs
+            if (!(BlenderInterface.IsAvailable && Config.GetValue(Enabled)))
             {
-                throw new ArgumentException("Imported model cannot have unicode characters in its file name.");
+                NotificationMessage.SpawnTextMessage(
+                    "Blender was not installed or detected.\nSVG Importer will not run", 
+                    colorX.Red);
+                return true;
             }
 
-            var normalizedExtension = Path.GetExtension(model).Replace(".", "").ToLower();
-            var trueCachePath = Path.Combine(Engine.Current.CachePath, "Cache");
-            var time = DateTime.Now.Ticks.ToString();
-
-            if (normalizedExtension == SVG_FILE_EXTENSION && BlenderInterface.IsAvailable)
+            List<string> notSVG = new();
+            foreach (var file in files)
             {
-                var blenderTarget = Path.Combine(trueCachePath, $"{modelName}_v2_{time}.glb").Replace("\'", "/");
-                SVGToGLB(model, blenderTarget);
-                __result = blenderTarget;
-                return;
+                var fileName = Path.GetFileName(file);
+                if (!fileName.ToLower().EndsWith(SVG_FILE_EXTENSION))
+                {
+                    notSVG.Add(file);
+                    continue;
+                }
+
+                if (ContainsUnicodeCharacter(file))
+                {
+                    Warn("Imported SVG cannot have unicode characters in its file name.");
+                    continue;
+                }
+
+                var blenderInput = file.Replace("\\", "/");
+                var blenderOutput = Path.Combine(TrueCachePath, $"{fileName}.glb").Replace("\\", "/");
+
+                var root = Engine.Current.WorldManager.FocusedWorld.RootSlot;
+                root.StartGlobalTask(async delegate
+                {
+                    ProgressBarInterface pbi = await root.World.
+                        AddSlot("Import Indicator").
+                        SpawnEntity<ProgressBarInterface, LegacySegmentCircleProgress>
+                            (FavoriteEntity.ProgressBar);
+                    pbi.Slot.PositionInFrontOfUser();
+                    pbi.Initialize(canBeHidden: true);
+                    pbi.UpdateProgress(0.0f, "Got SVG! Starting conversion...", string.Empty);
+
+                    await default(ToBackground);
+                    await SVGToGLB(blenderInput, blenderOutput, pbi);
+                    await default(ToWorld);
+
+                    pbi.ProgressDone("Conversion compete!");
+                    pbi.Slot.RunInSeconds(2.5f, delegate
+                    {
+                        pbi.Slot.Destroy();
+                    });
+
+                    UniversalImporter.Import(
+                        AssetClass.Model,
+                        new[] { blenderOutput },
+                        world,
+                        position,
+                        rotation);
+                });
             }
+
+            if (notSVG.Count <= 0) return false;
+            files = notSVG.ToArray();
+            return true;
         }
 
         private static bool ContainsUnicodeCharacter(string input)
@@ -63,17 +117,31 @@ public class SVGImporter : ResoniteMod
             return input.Any(c => c > MaxAnsiCode);
         }
 
-        private static void SVGToGLB(string input, string output)
+        private static async Task SVGToGLB(string input, string output, IProgressIndicator pbi)
         {
-            // Yes, deleting the default cube is necessary for this script to function
-            RunBlenderScript($"import bpy\nbpy.ops.import_curve.svg(filepath = '{input}')\nobjs = bpy.data.objects\ntry:    objs.remove(objs['Cube'], do_unlink = True)\nexcept:    pass\nfor obj in bpy.data.objects:\n    if type(obj.data) == 'Curve':\n        bpy.context.view_layer.objects.active = obj\n        bpy.ops.object.convert(target = 'MESH')\nbpy.ops.export_scene.gltf(filepath = '{output}')");
+            // Deleting the default cube is necessary for this script to function
+            // Last tested against Blender 3.5.0
+            await RunBlenderScript(@$"import bpy
+bpy.ops.import_curve.svg(filepath = '{input}')
+objs = bpy.data.objects
+try:    
+    objs.remove(objs['Cube'], do_unlink = True)
+except:
+    pass
+for obj in bpy.data.objects:
+    if type(obj.data).__name__ == 'Curve':
+        bpy.context.view_layer.objects.active = obj
+        obj.select_set(True)
+        bpy.ops.object.convert(target = 'MESH')
+        obj.select_set(False)
+bpy.ops.export_scene.gltf(filepath = '{output}')", pbi);
         }
 
-        private static void RunBlenderScript(string script, string arguments = "-b -P \"{0}\"")
+        private static async Task RunBlenderScript(string script, IProgressIndicator pbi)
         {
             var tempBlenderScript = Path.Combine(Path.GetTempPath(), Path.GetTempFileName() + ".py");
             File.WriteAllText(tempBlenderScript, script);
-            var blenderArgs = string.Format(arguments, tempBlenderScript);
+            var blenderArgs = string.Format("-b -P \"{0}\"", tempBlenderScript);
             blenderArgs = "--disable-autoexec " + blenderArgs;
 
             var process = new Process();
@@ -83,16 +151,16 @@ public class SVGImporter : ResoniteMod
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.CreateNoWindow = true;
             process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            process.OutputDataReceived += OnOutput;
+            process.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
+            {
+                pbi.UpdateProgress(0.0f, e.Data, string.Empty);
+                Msg(e.Data);
+            };
             process.Start();
             process.BeginOutputReadLine();
-            process.WaitForExit();
+            await process.WaitForExitAsync();
 
             File.Delete(tempBlenderScript);
-        }
-        private static void OnOutput(object sender, DataReceivedEventArgs e)
-        {
-            Msg(e.Data);
         }
     }
 }
---
+++ SVGImporter/TaskExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SVGImporter;
+
+internal static class TaskExtensions
+{
+    /// <summary>
+    /// Waits asynchronously for the process to exit.
+    /// https://stackoverflow.com/questions/470256/process-waitforexit-asynchronously
+    /// </summary>
+    /// <param name="process">The process to wait for cancellation.</param>
+    /// <param name="cancellationToken">A cancellation token. If invoked, the task will return 
+    /// immediately as canceled.</param>
+    /// <returns>A Task representing waiting for the process to end.</returns>
+    public static Task WaitForExitAsync(this Process process,
+        CancellationToken cancellationToken = default)
+    {
+        if (process.HasExited) return Task.CompletedTask;
+
+        var tcs = new TaskCompletionSource<object>();
+        process.EnableRaisingEvents = true;
+        process.Exited += (sender, args) => tcs.TrySetResult(null);
+        if (cancellationToken != default)
+            cancellationToken.Register(() => tcs.SetCanceled());
+
+        return process.HasExited ? Task.CompletedTask : tcs.Task;
+    }
+}
