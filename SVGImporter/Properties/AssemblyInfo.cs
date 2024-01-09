71fa92f0ec4e9d1a6dbb2058d61c263a59d8d34e
Benjamin Evans <evansBM@protonmail.com>
2024-01-09 10:12:04 -0700

    Bump assembly info

--- SVGImporter/Properties/AssemblyInfo.cs
+++ SVGImporter/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModNameGoesHere")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SVGImporter")]
+[assembly: AssemblyDescription("Imports .svg's into Resonite via Blender")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModNameGoesHere")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCompany("Hiatus Labs")]
+[assembly: AssemblyProduct("SVGImporter")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
